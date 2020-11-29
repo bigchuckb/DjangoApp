f70ee661e079ddf3a0dd3533224e396b16bfccae
bigchuckb <charlie.bank6@gmail.com>
2020-11-29 13:28:35 -0600

    updates

---
+++ webapp/settings_production.py
@@ -0,0 +1,12 @@
+from webapp.settings import *
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+STATISFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
