c7823b8451c4e99be8804cf919f15968fee14d6b
bigchuckb <charlie.bank6@gmail.com>
2020-11-29 13:17:38 -0600

    bug fix

--- webapp/settings.py
+++ webapp/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles' STATICFILES_DIRS = (
+STATIC_ROOT = 'staticfiles' 
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'), 
     )
 
